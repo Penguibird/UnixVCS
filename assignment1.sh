a2d39308aa33f923648c3e65dae14c94624be673
Vojtech Loskot <loskotaklp@gmail.com>
2021-11-01 14:31:47 +0000

    more stuff

--- assignment1.sh
+++ assignment1.sh
@@ -9,7 +9,7 @@ function menu (){
     echo "4: Check in a file"
     echo "5: Restore a file"
     echo "6: Archive"
-    echo "6: Read all logs"
+    echo "7: Read all logs"
     
     read input
     case $input in
@@ -44,7 +44,8 @@ function singleLinify () {
 
 # Replaces line break placeholder with actual linebreaks
 function multiLinify () {
-    echo -e "$(echo $1 | sed -z 's/NNNN/\n/g' | sed -z 's/RRRR/\n/g')"
+    echo -e "$(echo -e $1 | sed -z 's/NNNN/\n/g' | sed -z 's/RRRR/\n/g' | sed -z 's/CARRIAGERETURN/\r/g') "
+    echo " "
     #     printf \n
 }
 
@@ -60,8 +61,9 @@ function createReposit () {
         echo "created Reposit"
     fi
     mkdir $1/Logs
+    mkdir $1/backup
     touch $1/Logs/repo.log
-    echo "REPOCRT" $1, "Repository created by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") >> $dirName/Logs/repo.log
+    echo "REPOCRT" $1, "Repository created by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "NNNN" >> $dirName/Logs/repo.log
     menu
 }
 
@@ -83,7 +85,7 @@ function addFile () {
                 addFile
             fi
         else
-            echo "FILEADD" $fileName, "added by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "Changes: NNNN" $(singleLinify "$(<$fileName)" ) >> $dirName/Logs/repo.log
+            echo "FILEADD" $fileName, "added by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "NNNN     Changes: NNNN" $(singleLinify "$(<$fileName)") "NNNN CARRIAGERETURN" >> $dirName/Logs/repo.log
             cp $fileName $dirName/$fileName
             echo "Added file $fileName"
             menu
@@ -105,7 +107,7 @@ function checkOutFile () {
         PS3="Choose which file you want to checkout "
         select fileName in $(ls -p $dirName | grep -v /)
         do
-            echo "FCHKOUT" $fileName, "added by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") >> $dirName/Logs/repo.log
+            echo "FCHKOUT" $fileName, "added by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "NNNN" >> $dirName/Logs/repo.log
             echo "Checking out " $fileName
             #   https://stackoverflow.com/questions/8488253/how-to-force-cp-to-overwrite-without-confirmation
             yes | cp -rf $dirName/$fileName $fileName
@@ -125,7 +127,7 @@ function checkInFile () {
         ### Take action if $dirName exists ###
         echo "Choose which file you want to check in "
         read fileName
-        if [ -f "$dirName/$fileName" ]; then
+        if [ ! -f "$dirName/$fileName" ]; then
             echo "This file hasnt been added to the repository."
             echo "Would you like to add it?"
             read -p "(y - add file | n - choose a different file or repo)" option
@@ -135,15 +137,19 @@ function checkInFile () {
                 checkInFile
             fi
         fi
-        if [ -f "$fileName" ]; then
+        if [ ! -f "$fileName" ]; then
             echo "No such file exists."
             checkInFile
         else
-            echo "FCHKIN " $fileName, "added by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "Changes: NNNN" $(singleLinify "$(diff $fileName $dirName/$fileName)" ) >> $dirName/Logs/repo.log
+            echo "FCHKIN " $fileName, "File checked in by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "NNNN \t Changes: NNNN" $(singleLinify "$(diff $fileName $dirName/$fileName)") "NNNN CARRIAGERETURN" >> $dirName/Logs/repo.log
             echo "Checking in " $fileName
+            if [ ! -d $dirName/backup/$fileName ]; then
+                mkdir -p $dirName/backup/$fileName
+            fi
             mv $dirName/$fileName $dirName/backup/$fileName/$fileName_old_$(date +"%Y-%m-%d_%H-%M-%S")
             mv $fileName $dirName/$fileName
         fi
+        menu
     else
         ### Else if it doesn't ###
         echo "This repository doesn't exist, please re-enter a correct name"
@@ -160,7 +166,7 @@ function printLog () {
         ### Take action if $dirName exists ###
         # echo "$(multiLinify "$(<$dirName/Logs/repo.log)")"
         echo "Logs for the repo" $dirName
-        echo "LGPRNT" "printed by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") >> $dirName/Logs/repo.log
+        echo "LGPRNT" "Logs printed by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") "NNNN" >> $dirName/Logs/repo.log
         multiLinify "$(<$dirName/Logs/repo.log)"
         menu
     else
@@ -184,7 +190,7 @@ function restore () {
                 mv $dirName/backup/$fileName/$specificFileName $dirName/$fileName
             done
         done
-        echo "FILRST" $specificFileName, "restored by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") >> $dirName/Logs/repo.log
+        echo "FILRST" $specificFileName, "File restored by:" $(whoami), $(date +"%Y-%m-%d_%H-%M-%S") >> $dirName/Logs/repo.log
         echo "File $fileName restored. Ready to be checked out."
         menu
     else
